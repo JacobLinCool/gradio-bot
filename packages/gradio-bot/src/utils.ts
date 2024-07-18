e3f69ce9137691d0c6f05913f7f28f74168bf603
JacobLinCool <jacoblincool@gmail.com>
2024-07-18 21:39:05 +0800

    fix: multi-select list choices

--- packages/gradio-bot/src/utils.ts
+++ packages/gradio-bot/src/utils.ts
@@ -9,6 +9,11 @@ export function normalizeName(name: string) {
 }
 
 export function parseChoice(pythonType: string): string[] | undefined {
+	const matchList = pythonType.match(/^List\[(.*)\]$/);
+	if (matchList) {
+		pythonType = matchList[1];
+	}
+
 	const match = pythonType.match(/^Literal\['(.*)'\]$/);
 	if (!match) {
 		return undefined;
